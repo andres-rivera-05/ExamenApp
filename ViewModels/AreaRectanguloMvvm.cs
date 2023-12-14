04b839076d99b3031e1ddd8e7488c15aa061a42c
andres <148400031+andres-rivera-05@users.noreply.github.com>
2023-12-13 18:59:39 -0600

    Calculando area del rectangulo

---
+++ ViewModels/AreaRectanguloMvvm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using static Android.Provider.MediaStore.Audio;
+
+namespace ExamenApp.ViewModels
+{
+    public partial class AreaRectanguloMvvm : ObservableObject
+    {
+        [ObservableProperty]
+        public double bases;
+
+        [ObservableProperty]
+        public double altura;
+
+        [ObservableProperty]
+        public double area;
+
+        [RelayCommand]
+        public void CalcularAreaRectangulo()
+        {
+
+            if (Bases != 0 && Altura != 0)
+            {
+                Area = Bases * Altura;
+            }
+            else
+            {
+                Application.Current.MainPage.DisplayAlert("Error", "Completa los campos", "Aceptar");
+            }
+        
+        }
+
+        [RelayCommand]
+        public void Limpiar()
+        {
+            Bases = 0;
+            Altura = 0;
+            Area = 0;
+        }
+
+    }
+}
