fc8ee8138f8860afbbd78c10c0ec05e3792d4e09
andres <148400031+andres-rivera-05@users.noreply.github.com>
2023-12-13 19:00:11 -0600

    Calculando area circulo

--- ViewModels/AreaCirculoMvvm.cs
+++ ViewModels/AreaCirculoMvvm.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace ExamenApp.ViewModels
 {
@@ -16,9 +17,25 @@ namespace ExamenApp.ViewModels
         [ObservableProperty]
         public double radio;
 
-        [ObservableProperty]
-        public double resultado;
+        [RelayCommand]
+        public void CalcularAreaCirculo()
+        {
+            if (Radio != 0)
+            {
+                Area = Math.PI * Radio * Radio;
+            }
+            else
+            {
+                Application.Current.MainPage.DisplayAlert("Error", "EL campo del radio esta vacio", "Aceptar");
+            }
+        }
 
+        [RelayCommand]
+        public void Limpiar()
+        {
+            Radio = 0;
+            Area = 0;
+        }
 
     }
 }
